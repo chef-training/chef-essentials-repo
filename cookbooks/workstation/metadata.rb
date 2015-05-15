b199a30261e22c600f19e4e55e9a3666f0bd26b4
Franklin Webber <franklin.webber@gmail.com>
2015-05-15 14:55:32 -0700

    Update the metadata versions
    This is the position where the content ends on the first day.

--- cookbooks/workstation/metadata.rb
+++ cookbooks/workstation/metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'you@example.com'
 license          'all_rights'
 description      'Installs/Configures workstation'
 long_description 'Installs/Configures workstation'
-version          '0.1.0'
+version          '0.2.1'
 
