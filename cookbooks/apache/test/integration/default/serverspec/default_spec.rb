f338ae6bf78d70b728216f65d3810d949837efb9
Nathen Harvey <nharvey@chef.io>
2015-08-05 22:18:00 -0400

    Integration tests assume CentOS
    * CentOS package and service names
    * Expect syntax
    * Link to better serverspec docs

--- cookbooks/apache/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/apache/test/integration/default/serverspec/default_spec.rb
@@ -1,21 +1,27 @@
 require 'spec_helper'
 
 describe 'apache::default' do
-
   # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
+  # http://burtlo.github.io/serverspec.github.io/resource_types.html
 
   #
-  # @see http://serverspec.org/resource_types.html#port
+  # @see http://burtlo.github.io/serverspec.github.io/resource_types.html#port
   #
-  describe port(80) do
-    it { should be_listening }
+  it 'is listening on port 80' do
+    expect(port 80).to be_listening
   end
 
   #
-  # @see http://serverspec.org/resource_types.html#command
+  # @see http://burtlo.github.io/serverspec.github.io/resource_types.html#command
   #
-  describe command('curl http://localhost') do
-    its(:stdout) { should match /Hello, world!/ }
+  it 'serves a web page' do
+    expect(command('curl http://localhost').exit_status).to eq 0
+  end
+
+  #
+  # @see http://burtlo.github.io/serverspec.github.io/resource_types.html#command
+  #
+  it 'serves up the home page' do
+    expect(command('curl http://localhost').stdout).to match /Hello, world!/
   end
 end
