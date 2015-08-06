021b6b0a63673a0600e00e77ace20c0e5c3a54c0
Nathen Harvey <nharvey@chef.io>
2015-08-05 22:15:58 -0400

    rubocop prefers single quotes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,10 +5,10 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 #
-# The default recipe is the recipe that the cookbook is know most for 
-# accomplishing. It is often the practice to use this file to include specific 
+# The default recipe is the recipe that the cookbook is know most for
+# accomplishing. It is often the practice to use this file to include specific
 # recipes that perform a specific task.
 #
 # @see http://docs.chef.io/recipes.html#include-recipes
 #
-include_recipe "apache::server"
+include_recipe 'apache::server'
