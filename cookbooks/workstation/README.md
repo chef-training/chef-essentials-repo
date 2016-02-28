2e0edaa8d7f22c6bd3d3db7dba27a573e88f46c4
Franklin Webber <franklin.webber@gmail.com>
2016-02-28 16:26:13 -0600

    Create workstation cookbook with setup recipe

---
+++ cookbooks/workstation/README.md
@@ -0,0 +1,4 @@
+# workstation
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/workstation/metadata.rb
@@ -0,0 +1,7 @@
+name 'workstation'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures workstation'
+long_description 'Installs/Configures workstation'
+version '0.1.0'
