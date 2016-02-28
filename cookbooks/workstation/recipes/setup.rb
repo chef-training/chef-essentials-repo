ad7e0ec1d35e966bdb0f81adee2e98e07314a285
Franklin Webber <franklin.webber@gmail.com>
2016-02-28 16:43:21 -0600

    Update workstation setup recipe to use node attributes

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -11,13 +11,13 @@ package 'git' do
 end
 
 file '/etc/motd' do
-  content 'Property of ...
+  content "Property of ...
 
-  IPADDRESS: 104.236.192.102
-  HOSTNAME : banana-stand
-  MEMORY   : 502272 kB
-  CPU      : 2399.998 MHz
-'
+  IPADDRESS: #{node['ipaddress']}
+  HOSTNAME : #{node['hostname']}
+  MEMORY   : #{node['memory']['total']}
+  CPU      : #{node['cpu']['0']['mhz']}
+"
   mode '0644'
   owner 'root'
   group 'root'
