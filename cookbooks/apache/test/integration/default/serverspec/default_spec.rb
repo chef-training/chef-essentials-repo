f582413abe267bc6a7a99fbeab4554be86b8b22c
Franklin Webber <franklin.webber@gmail.com>
2016-02-28 16:39:00 -0600

    Add tests to apache cookbook default test suite

--- cookbooks/apache/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/apache/test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 describe 'apache::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  describe port(80) do
+    it { should be_listening }
+  end
+
+  describe command('curl http://localhost') do
+    its(:stdout) { should match /Hello, world!/ }
   end
 end
