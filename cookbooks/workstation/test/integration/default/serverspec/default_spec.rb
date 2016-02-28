72231187bb322f859ac09cdcf5945a424574e2da
Franklin Webber <franklin.webber@gmail.com>
2016-02-28 16:37:08 -0600

    Update workstation default test suite with more tests
    Test the tree package
    Test the Message of the Day file

--- cookbooks/workstation/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/workstation/test/integration/default/serverspec/default_spec.rb
@@ -4,4 +4,12 @@ describe 'workstation::default' do
   describe package('tree') do
     it { should be_installed }
   end
+
+  describe package('git') do
+    it { should be_installed }
+  end
+
+  describe file('/etc/motd') do
+    it { should be_owned_by 'root' }
+  end
 end
