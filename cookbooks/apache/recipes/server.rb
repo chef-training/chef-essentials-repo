fc7f787a1b4eb43147ffc8c5bbb2f0c1f631934d
Franklin Webber <franklin.webber@gmail.com>
2016-02-28 16:48:15 -0600

    Move the apache server's file resource to template

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -5,11 +5,8 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 package 'httpd'
 
-file '/var/www/html/index.html' do
-  content "<h1>Hello, world!</h1>
-<h2>ipaddress: #{node['ipaddress']}</h2>
-<h2>hostname: #{node['hostname']}</h2>
-"
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
 end
 
 service 'httpd' do
