138b027549d8ce25d92d52918fa62652c92ebf9b
Franklin Webber <franklin.webber@gmail.com>
2016-02-28 16:45:20 -0600

    Update apache server recipe to use node attribute and version 0.2.0

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -6,7 +6,10 @@
 package 'httpd'
 
 file '/var/www/html/index.html' do
-  content '<h1>Hello, world!</h1>'
+  content "<h1>Hello, world!</h1>
+<h2>ipaddress: #{node['ipaddress']}</h2>
+<h2>hostname: #{node['hostname']}</h2>
+"
 end
 
 service 'httpd' do
