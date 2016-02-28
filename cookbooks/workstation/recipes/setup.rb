3c5a10a0795f292bd3e8b6673116d105bd4e15be
Franklin Webber <franklin.webber@gmail.com>
2016-02-28 16:51:02 -0600

    Update workstation setup recipe to use template

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -10,14 +10,7 @@ package 'git' do
   action :install
 end
 
-file '/etc/motd' do
-  content "Property of ...
-
-  IPADDRESS: #{node['ipaddress']}
-  HOSTNAME : #{node['hostname']}
-  MEMORY   : #{node['memory']['total']}
-  CPU      : #{node['cpu']['0']['mhz']}
-"
+template '/etc/motd' do
   mode '0644'
   owner 'root'
   group 'root'
