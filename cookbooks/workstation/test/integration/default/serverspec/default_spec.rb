82869c91cb752852128c3a54b0cdd8b751883e3b
Franklin Webber <franklin.webber@gmail.com>
2016-02-28 16:35:24 -0600

    Update workstation default test suite first test

--- cookbooks/workstation/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/workstation/test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,7 @@
 require 'spec_helper'
 
 describe 'workstation::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  describe package('tree') do
+    it { should be_installed }
   end
 end
