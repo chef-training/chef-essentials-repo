d2ba0368f045c83ab428bdfda9a4dd8dbb1c7e8e
Franklin Webber <franklin.webber@gmail.com>
2016-02-28 16:42:18 -0600

    Update the workstation setup recipe with hardcoded system data

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -11,5 +11,14 @@ package 'git' do
 end
 
 file '/etc/motd' do
-  content 'Property of ...'
+  content 'Property of ...
+
+  IPADDRESS: 104.236.192.102
+  HOSTNAME : banana-stand
+  MEMORY   : 502272 kB
+  CPU      : 2399.998 MHz
+'
+  mode '0644'
+  owner 'root'
+  group 'root'
 end
