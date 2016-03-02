1a9c0f7fb18bdd227cd6b727c1dd53746d4e636e
Franklin Webber <franklin.webber@gmail.com>
2016-03-01 23:29:34 -0600

    Update the haproxy default recipe to use roles for web nodes

--- cookbooks/myhaproxy/recipes/default.rb
+++ cookbooks/myhaproxy/recipes/default.rb
@@ -4,17 +4,20 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-node.default['haproxy']['members'] = [{
-    'hostname' => 'knife node show node1 -a cloud.public_hostname',
-    'ipaddress' => 'knife node show node1 -a cloud.public_ips',
+all_web_nodes = search('node','role:web')
+
+members = []
+
+all_web_nodes.each do |web_node|
+  member = {
+    'hostname' => web_node['cloud']['public_hostname'],
+    'ipaddress' => web_node['cloud']['public_ipv4'],
     'port' => 80,
     'ssl_port' => 80
-  },
-  {
-      'hostname' => 'knife node show node3 -a cloud.public_hostname',
-      'ipaddress' => 'knife node show node3 -a cloud.public_ips',
-      'port' => 80,
-      'ssl_port' => 80
-    }]
+  }
+  members.push(member)
+end
+
+node.default['haproxy']['members'] = members
 
 include_recipe 'haproxy::default'
